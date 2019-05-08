6218566131d0eadb750406545c8e64d9c1ccc5e6
Spiderfav <ruifavinha2002@gmail.com>
2019-05-08 13:32:05 +0100

    Array Types
    All types of array in python up to 3D Arrays. May need to add 4D and ND.

---
+++ Test Array.py
@@ -0,0 +1,13 @@
+array1d = ["a","b","c","d"]
+#print(array1d[1])
+#print(array1d)
+
+array2d = [["a","b","c","d"],["e","f","g","h"]]
+#print(array2d[1][1])
+#print(array2d)
+
+array3d = [[["a","b","c","d"],["e","f","g","h"]],
+           [["i","j","k","l"],["m","n","o","p"]]]
+
+#print(array3d[1][1][1])
+#print(array3d)
